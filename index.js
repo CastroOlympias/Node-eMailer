6666885994534d1e9e7be06d883b6951fac0adf1
CastroOlympias <blackcarrera@msn.com>
2023-03-26 18:19:32 -0600

    got this working to send emails so created a repo and copied it here

---
+++ index.js
@@ -0,0 +1,36 @@
+const express = require('express')
+const nodemailer = require('nodemailer')
+const app = express()
+require('dotenv').config()
+const port = 3000
+
+app.listen(port, () => {
+    console.log(`nodemailerProject is listent at http://locahost:${port}`)
+})
+
+let transporter = nodemailer.createTransport({
+    service: 'gmail',
+    auth: {
+        type: 'OAuth2',
+        user: process.env.MAIL_USERNAME,
+        pass: process.env.MAIL_PASSWORD,
+        clientId: process.env.OAUTH_CLIENTID,
+        clientSecret: process.env.OAUTH_CLIENT_SECRET,
+        refreshToken: process.env.OAUTH_REFRESH_TOKEN
+    }
+});
+
+let mailOptions = {
+    from: 'tomerpacific@gmail.com',
+    to: 'blackcarrera@msn.com',
+    subject: 'Nodemailer Project',
+    text: 'Sent from Nodemailer Project @ 18:15'
+};
+
+transporter.sendMail(mailOptions, function (err, data) {
+    if (err) {
+        console.log("Error " + err);
+    } else {
+        console.log("Email sent successfully");
+    }
+});
